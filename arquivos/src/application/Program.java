ad1ed2fabdc442d89a87e444b7105c57956d7d42
Gleyson Miranda Souza <gleydson.m.s.1993@gmail.com>
2021-03-25 19:47:47 -0300

    25/03/2021

---
+++ arquivos/src/application/Program.java
@@ -0,0 +1,133 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+		
+		sc.close();
+	}
+
+	public static void aula05() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+
+		File path = new File(strPath);
+
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
+		}
+
+		boolean success = new File(strPath + "/subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+
+		sc.close();
+	}
+
+	public static void aula04() {
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+
+		String path = "/home/gleydson/devsuperior/javaudemy/out";
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void aula03() {
+		String path = "/home/gleydson/devsuperior/javaudemy/file";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+
+	public static void aula02() {
+		String path = "/home/gleydson/devsuperior/javaudemy/file";
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void aula01() {
+		File file = new File("/home/gleydson/devsuperior/javaudemy/file");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
+
+}
